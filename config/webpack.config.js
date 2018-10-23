41b5fc6f8a0418ae8865ddf60bb94c98c8d0b225
Tyreal Gray <tyrealgray@ymail.com>
2018-10-23 23:09:57 +0200

    fixed webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -25,8 +25,8 @@ module.exports = {
 						'@babel/preset-flow',
 					],
 					plugins: [
-						require('@babel/plugin-proposal-class-properties'),
-						require('@babel/plugin-proposal-object-rest-spread'),
+						'@babel/plugin-proposal-class-properties',
+						'@babel/plugin-proposal-object-rest-spread',
 					],
 				},
 			},
