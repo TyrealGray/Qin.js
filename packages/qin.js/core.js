a3ba06a74fbe75e39ba4e0d5fab3173c94cf5a88
Tyreal Gray <tyrealgray@ymail.com>
2020-04-12 23:07:24 +0100

    add perlin.js in qin.js

--- packages/qin.js/core.js
+++ packages/qin.js/core.js
@@ -1,6 +1,6 @@
 import { Qin } from './src/Qin';
 
-const qin = new Qin();
+const qin = new Qin({debugging: true});
 qin.createSandbox({ name: 'qindb' }).then(async () => {
 	await qin.start();
 });
