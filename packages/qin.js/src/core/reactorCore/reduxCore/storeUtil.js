948e43c06947aa93f55e2485b9ba50844a207ca8
TyrealGray <tyrealgray@ymail.com>
2021-03-23 18:23:13 +0000

    ignore RECORD_EVENT_TIME and eventTimeInfo in devtool

--- packages/qin.js/src/core/reactorCore/reduxCore/storeUtil.js
+++ packages/qin.js/src/core/reactorCore/reduxCore/storeUtil.js
@@ -4,5 +4,8 @@ import devToolsEnhancer from 'remote-redux-devtools';
 import { reducers } from './reducers';
 
 export function initStore(isDebugRedux: boolean): ReduxStore {
-	return createStore(combineReducers(reducers), devToolsEnhancer({ realtime: isDebugRedux, port: 1029 }));
+	return createStore(combineReducers(reducers), devToolsEnhancer({
+		realtime: isDebugRedux, port: 1029, actionsBlacklist: ['RECORD_EVENT_TIME'],
+		stateSanitizer: (state) => state.eventTimeInfo ? { ...state, eventTimeInfo: '<<EVENT_TIME>>' } : state,
+	}));
 }
